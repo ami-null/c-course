48072d1fa0c6772ffa314b5c6aae15785ef42c40
ami-null <70378126+ami-null@users.noreply.github.com>
2025-10-15 22:07:20 +0600

    add some array examples

---
+++ code-examples/07_01_accessing-array-element.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int a[5] = {10, 20, 30, 40, 50};
+    printf("%d\n", a[2]);   // prints 30
+    return 0;
+}
---
+++ code-examples/07_02_array-io.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    printf("How many numbers? ");
+    scanf("%d", &n);
+    int arr[100];    // assume max 100 for simplicity
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (i = 0; i < n; i++) {    // printing the values
+        printf("arr[%d] = %d\n", i, arr[i]);
+    }
+}
