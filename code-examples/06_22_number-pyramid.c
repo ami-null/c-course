f19f5de74b91463c80fb9f0d2964a3cf837b5b3d
ami-null <70378126+ami-null@users.noreply.github.com>
2025-10-24 15:31:02 +0600

    add nested loops code examples

---
+++ code-examples/06_22_number-pyramid.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n_rows = 5;
+    for (int i = 1; i <= n_rows; i++) {
+        for (int j = i; j < n_rows; j++){
+            printf(" ");   // initial spaces of each row
+        }
+        for (int k = 1; k <= i; k++){
+            printf("%d ", k);    // numbers of each row
+        }
+        printf("\n"); // newline after printing each row
+    }
+    return 0;
+}
