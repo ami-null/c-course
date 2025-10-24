a9b9c2e451dcfc94996eb52116b316ce1707eade
ami-null <70378126+ami-null@users.noreply.github.com>
2025-10-24 16:36:15 +0600

    add switch statement

---
+++ code-examples/06_23_switch_even-odd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    switch(n%2){
+        case 0:
+            printf("even\n");
+            break;
+        case 1:
+            printf("odd\n");
+            break;
+    }
+    return 0;
+}
