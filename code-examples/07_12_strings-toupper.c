a8245f0d2043db9d052e983f4c768ebdf971aa4b
Md. Aminul Islam Shazid <70378126+ami-null@users.noreply.github.com>
2025-11-18 12:52:02 +0600

    Create 07_12_strings-toupper.c

---
+++ code-examples/07_12_strings-toupper.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>    // for strlen()
+#include <ctype.h>    // for toupper(), tolower()
+
+int main(){
+    char s[] = "Hello World!";
+    for(int i = 0; i < strlen(s); i++){
+        s[i] = toupper((unsigned char)s[i]);
+    }
+    printf("%s", s);    // HELLO WORLD!
+    return 0;
+}
