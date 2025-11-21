13a85500d93d6281d40b6f4a261f26a006aa9057
Md. Aminul Islam Shazid <70378126+ami-null@users.noreply.github.com>
2025-11-21 20:59:29 +0600

    create header file example

---
+++ code-examples/08_08_using-header-file.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "utils.h"
+
+int main(){
+    sayHello();
+    printf("\n%d\n", add(3, 2));
+    printf("%f", area(1.5));
+}
---
+++ code-examples/utils.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define PI 3.142
+
+void sayHello(){
+   printf("Hello World");
+}
+
+int add(int a, int b){
+   return a+b;
+}
+
+float area(float radius){
+   return PI*radius*radius;
+}
