5d008caf5dbe2d18a838682a83ad68723ff4f1f7
Md. Aminul Islam Shazid <70378126+ami-null@users.noreply.github.com>
2025-11-25 19:53:17 +0600

    change %p to $d

--- code-examples/09_01_pointer-basics.c
+++ code-examples/09_01_pointer-basics.c
@@ -5,7 +5,7 @@ int main(){
     int *p = &x;
 
     printf("x = %d\n", x);
-    printf("Address of x = %p\n", p);
+    printf("Address of x = %d\n", p);
     printf("Value through pointer = %d\n", *p);
 
     return 0;
